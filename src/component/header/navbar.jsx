501809ca2e00cb673ec80e5ae7c745b1c38f0cb1
avinashveera <avinashveera77@gmail.com>
2022-08-21 04:57:25 -0400

    update

--- src/component/header/navbar.jsx
+++ src/component/header/navbar.jsx
@@ -14,17 +14,7 @@ export const Navbar = (props) => {
     const hdnav = document.getElementsByClassName("hidenav")
       hdnav[0].style.transform= "translateX(-100vw)";
   }
-const showcal=()=>{
-  const showc=document.getElementById("calend")
 
-  showc.style.transform="translate(-50% ,0%)"
-}
-
-const hidecal=()=>{
-  const showc=document.getElementById("calend")
-
-  showc.style.transform="translate(-50% ,-100%)"
-}
 
   return (
 
@@ -32,16 +22,7 @@ const hidecal=()=>{
      
       <div className='customnavbar'>
 
-        <div id='calend' style={{paddingTop:"50px",zIndex:"10",width:"200px",height:"200px",background:"rgb(2, 35, 91)",position:"absolute",left:"50%",transform:"translate(-50% ,-100%)",transition:"ease-in-out 600ms"}}>
-   <i onClick={hidecal} class="fa-solid fa-rectangle-xmark" style={{position:"absolute",top:"10px",right:"10px"}}></i>      
- {
-
- Date()
-
- }
-
-
-        </div>
+        
         
         <div className='navlogo' style={{ height: "100%" }}>
 
@@ -59,8 +40,7 @@ const hidecal=()=>{
 
         <div className='navlinks' style={{ width: "100%", height: "100%" }} >
 
-        <i onClick={showcal} className="fa-solid fa-calendar-day" style={{position:"absolute",top:"15px",left:"50%"}}></i>
-
+       
           <i onClick={shownav} className="fa-solid fa-bars togglemenu" style={{position:"absolute",right:"20px",top:"5px",padding:"10px"}}></i>
 
           <div onClick={hidenav} className="hidenav">
